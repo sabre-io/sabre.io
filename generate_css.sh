b12c4f5364cf2f5d00fcd9ab4c01c664963a0f0c
Evert Pot <evert@rooftopsolutions.nl>
2014-03-27 15:27:46 -0400

    Simplifying the less stuff.

--- generate_css.sh
+++ generate_css.sh
@@ -14,14 +14,6 @@ if ! type "yuicompressor" > /dev/null; then
     exit
 fi
 
-if [ ! -d "output_dev" ]; then
-    echo "The output_dev directory does not exist yet."
-    echo ""
-    echo "Please run:"
-    echo "sculpin generate"
-    exit
-fi;
-
 if [ $# -eq 0 ]; then
     echo "Usage: $0 [name of main less file without extension]"
     exit
@@ -35,20 +27,9 @@ if [ ! -f ./output_dev/less/$1.less ]; then
     exit
 fi
 
-cd output_dev
-
-# remove less.js from html
-find . -name "*.html" -exec sed -i '' '/less.min.js/ d' {} \;
-
-# replace less stylesheet with css
-find . -name "*.html" -exec sed -i '' 's/stylesheet\/less/stylesheet/ g' {} \;
-find . -name "*.html" -exec sed -i '' 's/less\/\(.*\).less/css\/\1.css/ g' {} \;
+cd source
 
 # compile and compress less files
-rm -rf css
-mkdir css
+rm css/*.css
 
 lessc ./less/$1.less | yuicompressor --type css > ./css/$1.css
-
-# remove less directory for deployment
-rm -rf less
